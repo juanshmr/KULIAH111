bde74e01548dbb265251a740667393ba822a6e65
Ajuwan <ajuwansahmir1@gmail.com>
2025-09-18 17:50:08 +0900

    Latihan pertemuan ke 4

---
+++ KULIAH/lib/ternate_map.dart
@@ -0,0 +1,14 @@
+void tampilkanWisata() {
+  print("🌊 Pantai Sulamadaha");
+  print("🏰 Benteng Kalamata");
+}
+
+void tampilkanKuliner() {
+  print("🐟 Ikan Bakar");
+  print("🍪 Kue Bagea");
+}
+
+void tampilkanEvent() {
+  print("🎉 Festival Tidore");
+  print("🎭 Pesta Rakyat");
+}
